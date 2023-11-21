16ddd826d621272dbe231e0199a9756199daedb6
emdevelopa <gyimahsunday@gmail.com>
2023-11-21 15:11:16 +0100

    nice

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const showDropdownIcon = document.getElementById("dropdown-show-icon");
 const hideDropdownIcon = document.getElementById("dropdown-hide-icon");
-
-const removePlan = document.getElementById("remove-plan-icon");
-const planPopUp = document.getElementById("plan-popup-container");
-
 const dropdown = document.getElementById("dropdown");
 
+const removePlan = document.getElementById("remove-plan-icon"); 
+const planPopUp = document.getElementById("plan-popup-container");
+
+
+// X-icon Event-Listener
 removePlan.addEventListener("click", () => {
     planPopUp.style.display = "none"
 })
 
+
+// Dropdown Event-Listener
 showDropdownIcon.addEventListener("click", () => {
   showDropdownIcon.style.display = "none";
   hideDropdownIcon.style.display = "block";
